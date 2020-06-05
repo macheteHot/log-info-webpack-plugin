09118f84e08d42e143fce25de81849a9d66abc8f
gugu <252819252@qq.com>
2020-06-05 14:49:51 +0800

    添加自定义打印/readme补充

--- README.md
+++ README.md
@@ -1,13 +1,47 @@
-### add console.log build info to index.html
 
-can log build info to console has Environment Build Date and version
+### 自动console.log编译信息，如date，NODE_ENV及其他自定义
 
-version need you config
++ 安装依赖
+```
+ npm install log-info-webpack-plugin -D
+ yarn add log-info-webpack-plugin -D
+```
 
-use:
++ vue.config.js配置使用
+```javascript
+const LogInfoPlugin = require('log-info-webpack-plugin')
 
-  plugins:[
 
-    new LogBuildInfo({version:'can require pacakge.json'})
+chainWebpack: config => {
+  config
+    .plugin('LogInfoPlugin')
+    .use(LogInfoPlugin, [{
+      key1:'value1', //可选,如“version”版本号，根据key/value打印
+      key2:'value2', //可选,如“version”版本号，根据key/value打印
+      // ...  more
+    }])
+}
+```
 
+或者 
+
+```javascript
+const LogInfoPlugin = require('log-info-webpack-plugin')
+
+{
+  // ... config settings here ...
+  plugins: [
+    new LogInfoPlugin({
+      key1:'value1', //可选,如“version”版本号，根据key/value打印
+      key2:'value2', //可选,如“version”版本号，根据key/value打印
+      // ...  more
+    })
   ]
+};
+```
+
+
++ 使用效果
+
+![效果图demo](./demo.jpg)
+
--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
-function getConsoleStr({ key, value }) {
+function getConsoleStr({
+  key,
+  value
+}) {
   const baseStyle = 'font-family: sans-serif;font-weight: bold;font-size: 12px;padding:5px;';
   const keyStyle = `color:#fff;background:#000;${baseStyle}border-radius:4px 0 0 4px`;
   const valueStyle = `color:#000;background:#FF9900;${baseStyle}border-radius:0 4px 4px 0`;
@@ -25,24 +28,24 @@ function formatDate(date) {
 }
 
 class LogInfo {
-  constructor(options) {
-    this.options = options;
+  constructor(logObj) {
+    this.logObj = logObj;
   }
 
   apply(compiler) {
     compiler.hooks.emit.tap('log-info-webpack-plugin', (compilation) => {
-      const { options } = this;
+      const { logObj } = this;
       return new Promise((resolve) => {
-        const { assets } = compilation;
+        const {
+          assets
+        } = compilation;
         let html = assets['index.html'].source();
-        const infoList = [
+        const baseList = [
           ['Environment', process.env.NODE_ENV],
           ['Build Date', formatDate(new Date())],
         ];
-        if (options.version) {
-          infoList.push(['Version', options.version]);
-        }
-        html = html.replace(/<\/body>/, `<script type="text/javascript">(function logTag(d){var b="font-family: sans-serif;font-weight: bold;font-size: 12px;padding:5px;";var c="color:#fff;background:#000;"+b+"border-radius:4px 0 0 4px";var a="color:#000;background:#FF9900;"+b+"border-radius:0 4px 4px 0";d.forEach(function(e){console.log("%c"+e[0]+"%c"+e[1],c,a)})})(${JSON.stringify(infoList)})</script></body>`);
+        const diyList = Object.keys(logObj).map(key => [key, logObj[key]])
+        html = html.replace(/<\/body>/, `<script type="text/javascript">(function logTag(d){var b="font-family: sans-serif;font-weight: bold;font-size: 12px;padding:5px;";var c="color:#fff;background:#000;"+b+"border-radius:4px 0 0 4px";var a="color:#000;background:#FF9900;"+b+"border-radius:0 4px 4px 0";d.forEach(function(e){console.log("%c"+e[0]+"%c"+e[1],c,a)})})(${JSON.stringify([...baseList,...diyList])})</script></body>`);
         assets['index.html'].source = () => html;
         assets['index.html'].size = () => html.size;
         resolve();
